3a8b573b0140c33657fa09b9077ddc93626b12f8
Raman Kumar <raman.10101@gmail.com>
2020-04-16 12:41:32 +0530

    Made Register Folder

---
+++ Register/UserImp.php
@@ -0,0 +1,24 @@
+<?php 
+
+class UserImp{
+
+    function __construct(){
+        require_once dirname(__FILE__) . '/UserQuery.php';
+    }
+
+
+	public function registerUser($first_name,$last_name,$username, $email, $password){
+		$userQuery = new UserQuery();
+		return $userQuery->registerUser($first_name,$last_name,$username, $email, $password);
+	}
+
+	        
+    public function loginUser($username,  $password){
+        $userQuery = new UserQuery();
+		return $userQuery->loginUser($username,  $password);
+	}
+
+
+}
+
+?>
