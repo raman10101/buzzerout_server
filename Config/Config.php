fff53b09409f7a7d47b6f3833dab2b343f83f755
lusiferjr <31337652+lusiferjr@users.noreply.github.com>
2020-04-18 16:49:19 +0530

    config comment my local connection

--- Config/Config.php
+++ Config/Config.php
@@ -3,5 +3,8 @@
  define('DB_PASSWORD','Rr8755025488$*');
  define('DB_NAME','u250918205_buzzerout');
  define('DB_HOST',"localhost");
-
+// define('DB_USERNAME','root');
+// define('DB_PASSWORD','');
+// define('DB_NAME','buzzerout');
+// define('DB_HOST',"localhost");
 ?>
